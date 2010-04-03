39f3d5a7ec5b7d634d149bc5abe7bd065798edb6
Aron Cedercrantz <aron@cedercrantz.se>
2010-04-03 02:15:29 +0200

    Added events class.

---
+++ CDEvents.m
@@ -0,0 +1,14 @@
+//
+//  CDEvents.m
+//  CDEvents
+//
+//  Created by Aron Cedercrantz on 03/04/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "CDEvents.h"
+
+
+@implementation CDEvents
+
+@end
